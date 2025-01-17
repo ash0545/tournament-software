ae0a2dd5bf13b818b70e3348f9ac4973b34b09e0
ashwin.santhosh2004 <112403369+ash0545@users.noreply.github.com>
2025-01-17 18:15:46 +0530

    feat(shadcn/ui): add pagination component
    - Add minor styles to tournament list page

--- tournament_app/app/tournaments/page.tsx
+++ tournament_app/app/tournaments/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Label } from "@/components/ui/label";
 import TournamentCard from "./TournamentCard";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 const mockData = {
   items: [
@@ -32,9 +35,25 @@ const mockData = {
 };
 
 export default function Page() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Tournaments</h1>
+    <div className=" m-10">
+      <div className="flex flex-row justify-between">
+        <Label
+          className={`font-semibold mb-[18px]`}
+          style={{ fontSize: "32px" }}
+        >
+          Tournaments
+        </Label>
+        <Button
+          onClick={() => router.push("/tournaments/create")}
+          className="max-w-[90px]"
+          type="button"
+        >
+          Create
+        </Button>
+      </div>
       <TournamentCard />
     </div>
   );
