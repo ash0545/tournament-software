417fbb17c251e527178bd656eb225ee8c35ad858
ashwin.santhosh2004 <112403369+ash0545@users.noreply.github.com>
2025-02-05 21:08:55 +0530

    feat: implement fetching individual tournament
    - Add hover shadow transition for tournament list cards

--- tournament_app/app/tournaments/TournamentList.tsx
+++ tournament_app/app/tournaments/TournamentList.tsx
@@ -3,16 +3,25 @@
 import { Tournament } from "@/components/lib/types";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 interface TournamentListProps {
   tournaments: Tournament[];
 }
 
 function TournamentList({ tournaments }: TournamentListProps) {
+  const router = useRouter();
+
   return (
     <div className="space-y-4">
       {tournaments.map((tournament) => (
-        <Card key={tournament.tournament_id}>
+        <Card
+          key={tournament.tournament_id}
+          className="shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out cursor-pointer"
+          onClick={() =>
+            router.push(`/tournaments/${tournament.tournament_id}`)
+          }
+        >
           <CardHeader>
             <CardTitle>{tournament.tournament_name}</CardTitle>
           </CardHeader>
--- tournament_app/app/tournaments/[tournament]/page.tsx
+++ tournament_app/app/tournaments/[tournament]/page.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
 
-async function Page({ params }: { params: Promise<{ tournament: string }> }) {
-  const tournamentID = (await params).tournament;
-  return <div>Test: {tournamentID}</div>;
+import { useFetchTournamentById } from "@/components/api/hooks/useTournaments";
+import React from "react";
+import { use } from "react";
+
+function Page({ params }: { params: Promise<{ tournament: string }> }) {
+  const t = use(params);
+  const tournamentId = t.tournament;
+
+  const { data, error, isLoading } = useFetchTournamentById(tournamentId);
+  return <div>Test: {data?.tournament_name}</div>;
 }
 
 export default Page;
